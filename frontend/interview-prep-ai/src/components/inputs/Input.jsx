abcedffc1183d791a3407bab1b253425b5048371
lazeicoder <anuragmukherjeedev@gmail.com>
2025-08-23 02:35:22 +0530

    Adding Basic Landing Page UI code

---
+++ frontend/interview-prep-ai/src/components/inputs/Input.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa6';
+
+const Input = ({ value, onChange, label, placeholder, type }) => {
+  return (
+    <div>Input</div>
+  )
+}
+
+export default Input;
