230dda1b85cf21f29a7d233d5154b6b7356846b0
lazeicoder <anuragmukherjeedev@gmail.com>
2025-08-25 03:07:44 +0530

    Adding Backend File structure

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+// Configuring .env file
+dotenv.config();
+
+// Creating an instance of express
+const app = express();
+
+// Middleware to handle CORS
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }) 
+);
+
+// Middleware to parse JSON content
+app.use(express.json());
+
+// Routes
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
