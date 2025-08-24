271941201e0899314e52f55af939228c7be01433
lazeicoder <anuragmukherjeedev@gmail.com>
2025-08-25 03:22:54 +0530

    Adding Basic Session Schema for our database

---
+++ backend/models/Session.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const SessionSchema = new mongoose.Schema(
+    {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        role: { type: String, required: true },
+        experience: { type: String, required: true },
+        topicsToFocus: { type: String, required: true },
+        description: String,
+        questions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Question" }]
+    },
+    {
+        timeseries: true 
+    }
+);
+
+module.exports = mongoose.model("Session", SessionSchema);
